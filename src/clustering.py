401abf95c9b958845864fd66880be361db4d3883
Josh Mantovani <josh.mantova@gmail.com>
2020-02-05 16:56:58 -0700

    Funcionized clustering.py

--- src/clustering.py
+++ src/clustering.py
@@ -23,6 +23,75 @@ from nltk.stem import SnowballStemmer
 import pyLDAvis
 import pyLDAvis.gensim
 
+
+def remove_stopwords(stopWords, descriptions):
+    cleaned_descriptions = []
+    for description in descriptions:
+        temp_list = []
+        for word in description.split():
+            if word not in stopWords:
+                temp_list.append(word.lower())
+        cleaned_descriptions.append(' '.join(temp_list))
+    return np.array(cleaned_descriptions)
+
+def remove_punctuation(descriptions):
+    no_punct_descriptions = []
+    for description in descriptions:
+        description_no_punct = ' '.join(RegexpTokenizer(r'\w+').tokenize(description))
+        no_punct_descriptions.append(description_no_punct)
+    return np.array(no_punct_descriptions)
+
+def get_wordnet_pos(word):
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {'J': wordnet.ADJ,
+               'N': wordnet.NOUN,
+               'V': wordnet.VERB,
+               'R': wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN)
+
+def lemmatize_descriptions(descriptions):
+    cleaned_descriptions = []
+    for description in descriptions:
+        temp_list = []
+        for word in description.split():
+            cleaned_word = WordNetLemmatizer().lemmatize(word, get_wordnet_pos(word))
+            temp_list.append(cleaned_word)
+        cleaned_descriptions.append(' '.join(temp_list))
+    return np.array(cleaned_descriptions)
+
+def clean_descriptions(stopWords, descriptions):
+    no_punct = remove_punctuation(descriptions)
+    no_punct_sw = remove_stopwords(stopWords, no_punct)
+    cleaned = lemmatize_descriptions(no_punct_sw)
+    return cleaned
+
+def get_representative_words(vectorizer, kmeans):
+    sorted_centroids = []
+    for cluster in kmeans.cluster_centers_:
+        top_10 = np.argsort(cluster)[::-1]
+        sorted_centroids.append(top_10[:10])
+    for idx, c in enumerate(sorted_centroids):
+        print(f'\nCluster {idx}\n')
+        for idx in c:
+            print(vectorizer.get_feature_names()[idx])
+
+def display_topics(model, feature_names, num_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print('Topic %d:' % (topic_idx))
+        print(' '.join([feature_names[i] for i in topic.argsort()[:-num_top_words - 1:-1]]))
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, get_wordnet_pos(text)))
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in stopWords and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+if __name__ == '__main__':
+
 # Reading in data
 df = pd.read_csv('../Datasets/df_all_linkedin.csv', index_col=0)
 df_co = pd.read_csv('../Datasets/df_linkedin_Colorado.csv', index_col=0)
@@ -48,49 +117,18 @@ stopWords = stopWords.union(add_stopwords)
 tokenize_remove_punct = RegexpTokenizer(r'\w+')
 lemma = WordNetLemmatizer()
 
-def remove_stopwords(stopWords, descriptions):
-    cleaned_descriptions = []
-    for description in descriptions:
-        temp_list = []
-        for word in description.split():
-            if word not in stopWords:
-                temp_list.append(word.lower())
-        cleaned_descriptions.append(' '.join(temp_list))
-    return np.array(cleaned_descriptions)
 
-def remove_punctuation(reg_tokenizer, descriptions):
-    no_punct_descriptions = []
-    for description in descriptions:
-        description_no_punct = ' '.join(reg_tokenizer.tokenize(description))
-        no_punct_descriptions.append(description_no_punct)
-    return np.array(no_punct_descriptions)
-
-def get_wordnet_pos(word):
-    tag = nltk.pos_tag([word])[0][1][0].upper()
-    tag_dict = {'J': wordnet.ADJ,
-               'N': wordnet.NOUN,
-               'V': wordnet.VERB,
-               'R': wordnet.ADV}
-    return tag_dict.get(tag, wordnet.NOUN)
-
-def lemmatize_descriptions(lemmatizer, descriptions):
-    cleaned_descriptions = []
-    for description in descriptions:
-        temp_list = []
-        for word in description.split():
-            cleaned_word = lemmatizer.lemmatize(word, get_wordnet_pos(word))
-            temp_list.append(cleaned_word)
-        cleaned_descriptions.append(' '.join(temp_list))
-    return np.array(cleaned_descriptions)
 
 # Cleaning descriptions for both the whole dataset and CO only
-descriptions_no_sw_co = remove_stopwords(stopWords, descriptions_co)
-descriptions_no_sw_punct_co = remove_punctuation(tokenize_remove_punct, descriptions_no_sw_co)
-cleaned_descriptions_co = lemmatize_descriptions(lemma, descriptions_no_sw_punct_co)
+cleaned_descriptions = clean_descriptions(stopWords, descriptions)
 
-descriptions_no_sw = remove_stopwords(stopWords, descriptions)
-descriptions_no_sw_punct = remove_punctuation(tokenize_remove_punct, descriptions_no_sw)
-cleaned_descriptions = lemmatize_descriptions(lemma, descriptions_no_sw_punct)
+# descriptions_no_sw_co = remove_stopwords(stopWords, descriptions_co)
+# descriptions_no_sw_punct_co = remove_punctuation(tokenize_remove_punct, descriptions_no_sw_co)
+# cleaned_descriptions_co = lemmatize_descriptions(lemma, descriptions_no_sw_punct_co)
+
+# descriptions_no_sw = remove_stopwords(stopWords, descriptions)
+# descriptions_no_sw_punct = remove_punctuation(descriptions_no_sw)
+# cleaned_descriptions = lemmatize_descriptions(descriptions_no_sw_punct)
 
 # Vectorizing words creating both tf and tf-idf matrices
 vectorizer = CountVectorizer(stop_words=stopWords, min_df=.15, max_df=0.75, max_features=5000)
@@ -103,15 +141,18 @@ kmeans = KMeans(n_clusters=5, verbose=True, n_jobs=-1)
 kmeans.fit(tfidf)
 
 # Returning most representative words for each cluster
-sorted_centroids = []
-for cluster in kmeans.cluster_centers_:
-    top_10 = np.argsort(cluster)[::-1]
-    sorted_centroids.append(top_10[:10])
 
-for idx, c in enumerate(sorted_centroids): 
-    print(f'\nCluster {idx}\n')
-    for idx in c: 
-        print(tfidf_vectorizer.get_feature_names()[idx]) 
+
+get_representative_words(tfidf_vectorizer, kmeans)
+# sorted_centroids = []
+# for cluster in kmeans.cluster_centers_:
+#     top_10 = np.argsort(cluster)[::-1]
+#     sorted_centroids.append(top_10[:10])
+
+# for idx, c in enumerate(sorted_centroids): 
+#     print(f'\nCluster {idx}\n')
+#     for idx in c: 
+#         print(tfidf_vectorizer.get_feature_names()[idx]) 
 
 # Calculating model score for kmeans
 silhouette_score(tfidf, kmeans.labels_)
@@ -127,10 +168,7 @@ lda = LatentDirichletAllocation(n_components=4,
 lda.fit(tf)
 
 # Displaying most representative words for each cluster of LDA
-def display_topics(model, feature_names, num_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print('Topic %d:' % (topic_idx))
-        print(' '.join([feature_names[i] for i in topic.argsort()[:-num_top_words - 1:-1]]))
+
         
 num_top_words=10
 display_topics(lda, feature_names, num_top_words)
@@ -140,16 +178,7 @@ display_topics(lda, feature_names, num_top_words)
 data_text = df[['Description']].copy()
 data_text['index'] = data_text.index
 documents = data_text
-def lemmatize_stemming(text):
-    return WordNetLemmatizer().lemmatize(text, get_wordnet_pos(text))
-#     return stemmer.stem(WordNetLemmatizer().lemmatize(text, get_wordnet_pos(text)))
 
-def preprocess(text):
-    result = []
-    for token in gensim.utils.simple_preprocess(text):
-        if token not in stopWords and len(token) > 3:
-            result.append(lemmatize_stemming(token))
-    return result
 
 stemmer = SnowballStemmer('english')
 processed_docs = documents['Description'].map(preprocess)
@@ -165,4 +194,4 @@ lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=3, id2word=id2word
 
 # Visualizing LDA with PyLDAvis
 vis = pyLDAvis.gensim.prepare(lda_model, bow_corpus, id2word)
-vis
+pyLDAvis.show(vis)
